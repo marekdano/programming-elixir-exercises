f64f90058c9ef04177c106559ae0ea454323b2fe
Marek Dano <mk.dano@gmail.com>
2016-10-08 23:45:28 +0100

    Add a solution of the exercise 1 to the chapter 7.

---
+++ ch-07/listsAndRecursion-1.exs
@@ -0,0 +1,25 @@
+defmodule MyList do
+
+  def reduce([], value, _) do 
+    value 
+  end
+  def reduce([head | tail], value, func) do
+    reduce(tail, func.(head, value), func)  
+  end
+
+  # Write a function mapsum that takes a list and a function. 
+  # It applies the function to each element of the list, and then sums the result, so
+  # MyList.mapsum [1, 2, 3], &1 * &1  => 14
+
+  def mapsum([], _func), do: 0
+  def mapsum([head | tail], func) do
+    func.(head) + mapsum(tail, func)
+  end
+
+end
+
+IO.puts MyList.reduce([1,2,3,4,5], 0, &(&1 + &2))
+IO.puts MyList.reduce([1,2,3,4,5], 1, &(&1 * &2))
+
+IO.puts MyList.mapsum([1, 2, 3], &1 * &1)
+
